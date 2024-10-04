d5632cbc2d639a08717102c90c4c4ae21505f224
13aba <bayarsaikhan.bayarbat@gmail.com>
2024-10-04 14:25:00 +0800

    -finished footer
    -Footer finished with grid layout
    -Added scroll to top button and logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,10 @@
 
 
 window.addEventListener('scroll', () => {
+    // Dynamic navigation menu
     // Retrieve the navigation menu from the DOM
     const navBar = document.querySelector('.nav-bar');
     const companyLogo = document.querySelector('.company-logo-scroll');
-
-    // Compute the distance from the top of the page
-    let offsetTop = navBar.offsetTop;
 
     // Make navigation menu fixed to the top of the screen when a certain vertical scroll threshold is reached
     navBar.classList.toggle('sticky', window.scrollY > navBar.offsetTop);
@@ -14,4 +12,19 @@ window.addEventListener('scroll', () => {
     // Display the company logo in a white box in the top left corner of the fixed navigation menu when a 
     // vertical scroll threshold is reached 
     companyLogo.classList.toggle('company-logo-scroll-active', window.scrollY > navBar.offsetTop);
+
+
+    // Scroll to top button
+    const scrollUp = document.querySelector('.scroll-up-button');
+    // Display scroll button when scrolled 
+    scrollUp.classList.toggle('visible', window.scrollY > navBar.offsetTop);
+    //Scroll to top when pressed
+    scrollUp.addEventListener("click", function() {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+            });
+            
+        });
 })
